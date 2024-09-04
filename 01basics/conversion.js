3c80486893b3cb9be665a0f52d69391bf51e8c6f
Vivek8840 <167604912+Vivek8840@users.noreply.github.com>
2024-09-04 09:39:20 +0000

    -m "Datatypes and conversion"

---
+++ 01basics/conversion.js
@@ -0,0 +1,25 @@
+let score=true;
+// const{score} =req.bodyw
+// console.log(typeof score)
+// console.log(typeof(score))
+let valueInnum= Number(score)
+// console.log(typeof valueInnum)
+// console.log(valueInnum);
+
+//on conversion
+//"33"=> 33
+//"33abc"=> NaN - Not a number
+//true => 1
+// false => 0
+// null =>0
+// undefined => NaN
+let isLoggedin="ram";
+let booleanIsloggedin = Boolean(isLoggedin)
+// console.log(booleanIsloggedin)
+// console.log(isLoggedin)
+// 1=> true; 0 => false
+// ""=>false; "ram"=> true
+let num=33;
+let stringnum=String(num);
+console.log(stringnum);
+console.log(typeof stringnum)
---
+++ 01basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all Js code as newer version
+// alert("Hello");
+// alert(3+3); We are using Node JS not browser
+console.log(3+3);
+console.log("Hello");
+//Make sure the code is readable
+let name ="Vivek";
+let age=19;
+let isloggedIn=false
+
+
+//Datatypes
+//numbr => 2to power 53
+//bigInt
+//string=>""
+//boolean =>true/false
+//null =>it is a standalone value -- it representation of empty value
+//undefined => value is not assinged yet
+// symbol => unique 
+
+
+
+
+// Object =>
+console.log(typeof"Vivek");
+console.log(typeof null);
+console.log(typeof age);
+console.log(typeof undefined)
